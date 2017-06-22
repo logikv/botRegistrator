dea331d299dcf44ecf21178a0471de0c3b34730d
logikv <logikv@users.noreply.github.com>
2017-06-22 18:16:32 +0300

    getProfile
    add new method

---
+++ getProfile.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bva
+ * Date: 22.06.2017
+ * Time: 18:10
+ */
+
+
+$imgNum = 'bind';
+$sid = $_GET['sid'];
+$imgNum = $_GET['imgNum'];
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+
+    $result = qb()->table('botresource')->where(array(
+        'check' => '1'
+    ))->one();
+    echo $result['nickname'] . '' . $result['lastname'] . '' . $result['firstname'];
+    $bind = qb()->table('profile_list')->where(array('nickname' => $result['nickname']))->update(array($sid => $imgNum));
+
+
+}
+
+?>
