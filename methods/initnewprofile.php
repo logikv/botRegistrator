156b30e4c52d0ea85e86afc456b840385feb81ad
logikv <logikv@yandex.ru>
2017-06-22 12:29:32 +0300

    change methods and libs

--- methods/initnewprofile.php
+++ methods/initnewprofile.php
@@ -15,7 +15,7 @@ $profileFirstName = $_GET['profileFirstName'];
 
 
 //$query = "INSERT INTO botresource (nickname, status, lastname, firstname) VALUES ('$profileNickname', '$profileStatus', '$profileLastName', '$profileFirstName')";
-$query = "INSERT INTO botresource (nickname, lastname, firstname) VALUES ('$profileNickname', '$profileLastName', '$profileFirstName')";
+//$query = "INSERT INTO botresource (nickname, lastname, firstname) VALUES ('$profileNickname', '$profileLastName', '$profileFirstName')";
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -26,20 +26,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         if ($result = qb()->table('botresource')->insert(array(
             'nickname' => $profileNickname,
             'lastname' => $profileLastName,
-            'firstname' => $profileFirstName
-        ))
+            'firstname' => $profileFirstName))
         ) {
-            mkdir("/var/www/html/downloads/$profileNickname/", 1777);
-            $addNewProfile = "success";
-            echo $addNewProfile;
-        }
-
-        if ($result = $mysqli->query("$query", MYSQLI_USE_RESULT)) {
 
             mkdir("/var/www/html/downloads/$profileNickname/", 1777);
             $addNewProfile = "success";
             echo $addNewProfile;
-            //          $result->close();
         } else {
             echo "profile already exist";
         }
@@ -47,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     } else {
         echo '$profileNickname не задан. $profileNickname является ключевым полем для далнейшего обращения';
     }
-}
 
+}
 
 ?>
